e22b7bd557b35e5baf6b2ebdcdf6888c4c7ed0b6
Hardik Taluja <hardiktaluja8@gmail.com>
2018-07-29 14:20:30 +0530

    Improved OnClickListener, Added Back Button, Added drawables at multiple densities

--- app/src/main/java/com/example/android/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,39 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout bouquet = (LinearLayout) findViewById(R.id.bouquet);
-        bouquet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BouquetActivity.class);
-                startActivity(intent);
-            }
-        });
+        bouquet.setOnClickListener(this);
 
         LinearLayout collage = (LinearLayout) findViewById(R.id.collage);
-        collage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CollageActivity.class);
-                startActivity(intent);
-            }
-        });
+        collage.setOnClickListener(this);
 
         LinearLayout colour = (LinearLayout) findViewById(R.id.colour);
-        colour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ColourActivity.class);
-                startActivity(intent);
-            }
-        });
+        colour.setOnClickListener(this);
 
         LinearLayout listen = (LinearLayout) findViewById(R.id.listen);
-        listen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListenActivity.class);
-                startActivity(intent);
-            }
-        });
+        listen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.bouquet:
+                Intent bouquetIntent = new Intent(MainActivity.this, BouquetActivity.class);
+                startActivity(bouquetIntent);
+                break;
+
+            case R.id.collage:
+                Intent collageIntent = new Intent(MainActivity.this, CollageActivity.class);
+                startActivity(collageIntent);
+                break;
+
+            case R.id.colour:
+                Intent colourIntent = new Intent(MainActivity.this, ColourActivity.class);
+                startActivity(colourIntent);
+                break;
+
+            case R.id.listen:
+                Intent listenIntent = new Intent(MainActivity.this, ListenActivity.class);
+                startActivity(listenIntent);
+                break;
+        }
+
     }
 }
